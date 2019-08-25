17b1cd5631f41a543f1276a910b4340f0a85b3d2
bouzuya <m@bouzuya.net>
2019-08-25 14:33:03 +0900

    add Home -> Input navigation tests

--- app/src/androidTest/java/net/bouzuya/sample2/HomeFragmentTest.kt
+++ app/src/androidTest/java/net/bouzuya/sample2/HomeFragmentTest.kt
@@ -1,23 +1,32 @@
 package net.bouzuya.sample2
 
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onIdle
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
+import net.bouzuya.sample2.HomeFragmentDirections.Companion.actionHomeToInput
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 
 class HomeFragmentTest {
-    lateinit var viewModel: HomeViewModel
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var goToInputEvent: MutableLiveData<Unit>
 
     @Before
     fun setUp() {
         viewModel = mock(HomeViewModel::class.java)
+        goToInputEvent = MutableLiveData()
+        `when`(viewModel.goToInputEvent).thenReturn(goToInputEvent) // observe it
 
         val application =
             InstrumentationRegistry.getInstrumentation().targetContext.applicationContext as Sample2Application
@@ -32,5 +41,28 @@ class HomeFragmentTest {
         launchFragmentInContainer<HomeFragment>()
 
         onView(withId(R.id.home_name_text_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.home_go_to_input_button)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun clickGoToInputButton() {
+        launchFragmentInContainer<HomeFragment>()
+
+        onView(withId(R.id.home_go_to_input_button)).perform(click())
+
+        verify(viewModel).goToInput()
+    }
+
+    @Test
+    fun observeGoToInputEvent() {
+        val navController = mock(NavController::class.java)
+        launchFragmentInContainer<HomeFragment>().onFragment { fragment ->
+            Navigation.setViewNavController(fragment.view!!, navController)
+        }
+
+        goToInputEvent.postValue(Unit) // fire event in main thread
+        onIdle() // wait event handling
+
+        verify(navController).navigate(actionHomeToInput())
     }
 }
